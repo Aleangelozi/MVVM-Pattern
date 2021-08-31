206ddc2dd2d23153c3414723bf3ed0d933cbf917
aleangelozi <aleangelozi@gmail.com>
2021-08-30 23:21:52 -0300

    WIP

--- app/src/main/java/com/aleangelozi/mvvm_pattern/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/mvvm_pattern/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.aleangelozi.mvvm_pattern
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.aleangelozi.mvvm_pattern.view.ViewActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, ViewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
